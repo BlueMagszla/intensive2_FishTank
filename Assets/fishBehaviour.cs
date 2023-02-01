ba8833df9d7871ee8cfe419699705df704a4faa0
SlySlakva <93298678+SlySlakva@users.noreply.github.com>
2023-01-31 21:26:05 -0500

    fish behaviour working :D
    fishies are fishing

--- Assets/fishBehaviour.cs
+++ Assets/fishBehaviour.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class FishBehaviour : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float normalSpeed = 1.0f;
+    public float chaseSpeed = 2.0f;
+    public float fleeSpeed = 3.0f;
     public float frequency = 1.0f;
     public float magnitude = 0.1f;
     public float changeDirectionTime = 5.0f;
+    public float fleeRadius = 10.0f;
+    public float fleeDuration = 5.0f;
+    public float floatingForce = 5.0f;
 
-    Vector3 pos, localPosition;
+    Vector3 localPosition;
     float timer;
     Quaternion randomRotation;
+    float x;
 
     GameObject[] players;
     GameObject closestPlayer;
+    Vector3 previousPlayerPosition;
+    float fleeTimer;
+    Rigidbody rb;
 
     void Start()
     {
-        pos = transform.position;
         localPosition = transform.localPosition;
         timer = changeDirectionTime;
+        rb = GetComponent<Rigidbody>();
+        x = 0f;
     }
 
     void Update()
@@ -42,7 +52,25 @@ public class FishBehaviour : MonoBehaviour
                 }
             }
 
-            transform.LookAt(closestPlayer.transform);
+            if (closestDistance <= fleeRadius && closestPlayer.transform.position != previousPlayerPosition)
+            {
+                randomRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+                transform.rotation = randomRotation;
+                previousPlayerPosition = closestPlayer.transform.position;
+                fleeTimer = fleeDuration;
+                rb.velocity = transform.forward * fleeSpeed;
+            }
+            else if (fleeTimer > 0)
+            {
+                fleeTimer -= Time.deltaTime;
+                rb.velocity = transform.forward * fleeSpeed;
+            }
+            else
+            {
+                transform.LookAt(closestPlayer.transform);
+                previousPlayerPosition = closestPlayer.transform.position;
+                rb.velocity = transform.forward * chaseSpeed;
+            }
         }
         else
         {
@@ -53,10 +81,13 @@ public class FishBehaviour : MonoBehaviour
                 transform.rotation = randomRotation;
                 timer = changeDirectionTime;
             }
+
+            rb.velocity = transform.forward * normalSpeed;
         }
 
-        pos += transform.forward * Time.deltaTime * speed;
-        localPosition.y = Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * magnitude;
-        transform.position = pos + localPosition;
+        x += Time.deltaTime * frequency;
+        float y = Mathf.PerlinNoise(x, 0f) * magnitude;
+        rb.position = new Vector3(rb.position.x, localPosition.y + y, rb.position.z);
+        rb.AddForce(Vector3.up * floatingForce);
     }
 }
